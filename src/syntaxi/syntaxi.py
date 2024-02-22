07df93f2a87d036d8ed5b49ae2f3a13431477cde
Niels Horn <slein.nroh@gmail.com>
2024-02-22 09:43:36 +0100

    Batch operations.

--- src/syntaxi/syntaxi.py
+++ src/syntaxi/syntaxi.py
@@ -23,20 +23,32 @@ def patched_tokenizer(tokenizer):
     original_encode = new_tokenizer.encode
     original_decode = tokenizer.decode
 
-    def patched_encode(self, text: str | list[str], **kwargs):
-        if isinstance(text, list):
-            processed_texts = [convert_to_shift_tokens(text=text) for text in text]
-        else:
-            processed_texts = [convert_to_shift_tokens(text=text)]
+    original_encode_batch = new_tokenizer.encode_batch
+    original_decode_batch = tokenizer.decode_batch
+
+    def patched_encode(self, text: str, **kwargs):
+        processed_texts = convert_to_shift_tokens(text=text)
 
         kwargs["add_special_tokens"] = True
         return original_encode(processed_texts, **kwargs)
 
-    def patched_decode(self, text: list[str], **kwargs) -> str:
+    def patched_decode(self, text: list[int], **kwargs) -> str:
         return convert_from_shift_tokens(original_decode(text, **kwargs))
+
+    def patched_encode_batch(self, texts: list[str], **kwargs):
+        processed_texts = [convert_to_shift_tokens(text=text) for text in texts]
+
+        kwargs["add_special_tokens"] = True
+        return original_encode_batch(processed_texts, **kwargs)
+
+    def patched_decode_batch(self, text: list[list[int]], **kwargs) -> list[str]:
+        return [convert_from_shift_tokens(text) for text in original_decode_batch(text, **kwargs)]
 
     new_tokenizer.encode = patched_encode.__get__(tokenizer)
     new_tokenizer.decode = patched_decode.__get__(tokenizer)
+
+    new_tokenizer.encode_batch = patched_encode_batch.__get__(tokenizer)
+    new_tokenizer.decode_batch = patched_decode_batch.__get__(tokenizer)
 
     return new_tokenizer
 
